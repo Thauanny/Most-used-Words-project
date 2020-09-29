3991f9e3f86e553a6ce8de2c630a32a70b907d25
thauanny <thauannyk@ufrn.edu.br>
2020-09-29 12:09:26 -0300

    frontend

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container fluid>
+      <br>
+        <v-form>
+          <v-file-input 
+
+          label="Selecione as Legendas" 
+          prepend-icon="mdi-message-text" 
+          append-outer-icon="mdi-send" 
+          outlined 
+          multiple 
+          chips 
+          v-model="files"
+          @click:append-outer = "processSubtitles"
+          />
+
+        </v-form>
+
+        <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amout="word.amout"/>
+        </div>
+    </v-container>
+</template>
+
+<script>
+
+import Pill from './Pill'
+
+export default {
+
+    components:{Pill},
+
+    data: function() {
+        return {
+          files: [],
+          groupedWords: [
+            { name: 'why', amout: 1234},
+            { name: 'you', amout: 900},
+            { name: 'he', amout: 853},
+          ]
+        }
+    },
+    methods: {
+      processSubtitles(){
+        console.log(this.files)
+      }
+    }
+}
+</script>
+
+<style>
+      .pills{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+
+      }
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="pill">
+        <span class="name">{{name}}</span>
+        <span class="amout">{{amout}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amout']
+}
+</script>
+
+<style scoped>
+    .pill{
+        margin: 10px;
+        height: 55px;
+        border-radius: 30px;
+
+        background-color: rgb(233, 192, 200);
+        color: #fff;
+        font-size: 1.3rem;
+        font-weight: bold;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .pill .name{
+        margin-left: 15px;
+    }
+
+    .pill  .amout{
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        widows: 35px;
+        border-radius: 18px;
+
+        background-color: white;
+        color:  rgb(233, 192, 200);
+        font-size: 14px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
